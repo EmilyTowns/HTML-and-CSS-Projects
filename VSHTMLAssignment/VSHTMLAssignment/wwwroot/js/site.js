93d531be2739d6faa4fc8451bb3b7e49d13396a1
Emily Towns <110913738+EmilyTowns@users.noreply.github.com>
2022-11-22 09:44:15 -0400

    VS HTML Assignment

---
+++ VSHTMLAssignment/VSHTMLAssignment/wwwroot/js/site.js
@@ -0,0 +1,14 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+function color1() {
+    document.getElementById("title").style.color = "aquamarine";
+}
+
+function color2() {
+    document.getElementById("title").style.color = "black";
+}
+
+
